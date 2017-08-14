4f4dae38e5ece3cf155f0f54c7b47ff2d3a178fe
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2017-08-14 10:50:39 +0300

    SoupmixAPCUCacheFactory factory added

---
+++ src/SoupmixAPCUCacheFactory.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace SelamiApp\Factories;
+
+use Zend\ServiceManager\Factory\FactoryInterface;
+use Interop\Container\ContainerInterface;
+use Psr\SimpleCache\CacheInterface;
+use Soupmix\Cache\APCUCache;
+
+class SoupmixAPCUCacheFactory implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null) : CacheInterface
+    {
+        return new APCUCache();
+    }
+}
